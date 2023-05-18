4fa8f026cfd6595e5862e6fde606c1ba65299eba
Mo Helmy <helmysmile3@gmail.com>
2023-05-18 22:01:46 +0200

    create user model, setup django admin and add api documentation

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,82 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+# class UserAdmin(admin.ModelAdmin):
+#     ordering = ['id']
+#     list_display = ('email', 'name', 'is_superuser')
+#     fieldsets = (
+#         (None, {'fields': ('email', 'password')}),
+#         (_('Personal Info'), {'fields': ('name',)}),
+#         (
+#             _('Permissions'),
+#             {
+#                 'fields': (
+#                     'is_active',
+#                     'is_staff',
+#                     'is_superuser',
+#                 )
+#             }
+#         ),
+#         (_('Important dates'), {'fields': ('last_login',)}),
+#     )
+#     readonly_fields = ['last_login']
+#     add_fieldsets = (
+#         (None, {
+#             'classes': ('wide',),
+#             'fields': (
+#                 'email',
+#                 'password1',
+#                 'password2',
+#                 'name',
+#                 'is_active',
+#                 'is_staff',
+#                 'is_superuser',
+#             ),
+#         }),
+#     )
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    list_display = ('email', 'name', 'is_superuser', )
+    ordering = ('id',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
